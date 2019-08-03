5070e4c8078c8290ad14f36550b921242ec0d3c3
Tantan4321 <kupa528@yahoo.com>
2019-08-02 21:21:32 -0500

    1.3: RelativeLayout Article display example

---
+++ app/src/main/java/com/tantan4321/demoreference/ScrollViewDemo.java
@@ -0,0 +1,14 @@
+package com.tantan4321.demoreference;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScrollViewDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_view_demo);
+    }
+}
